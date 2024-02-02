f600ee2c64da2e97435e47e1e835fe94bbfc6671
dcnguyen <dcnguyen@mines.edu>
2024-02-02 10:02:43 -0700

    removed commented out function
    removed unused function so i can try something idk

--- maze_input.py
+++ maze_input.py
@@ -2,35 +2,35 @@ import networkx as net
 import matplotlib.pyplot as plt
 import string
 
-# def drawRefurbishedGraph(graph, pos):
-#     # size of plot
-#     plt.figure(figsize=(10,10),dpi=100)
-#     # set up position of graph items
+def drawRefurbishedGraph(graph, pos):
+    # size of plot
+    plt.figure(figsize=(10,10),dpi=100)
+    # set up position of graph items
         
-#     # draw nodes
-#     for node in graph.nodes():
-#         net.draw_networkx_nodes(graph, pos,
-#                                nodelist=list(graph.nodes()),
-#                                node_size=250, node_color='red', alpha=0.9)
-#         print("drawn node:", node)
-#     # draw edges
-#     for edge in graph.edges:
-#         net.draw_networkx_edges(graph, pos,
-#                                edgelist=list(graph.edges()),
-#                                width=3, alpha=0.9, edge_color='grey')
-#         print("drawn edge:", edge)
-#     # draw network labels
-#     labels={}
-#     for node in graph.nodes():
-#         # label according to PDF problem statement node labeling standard
-#         if str(node) != 'win':
-#             labels[node]=str(node[0]+1)+", "+str(node[1]+1)
-#         else:
-#             labels['win'] = 'win'
-#     net.draw_networkx_labels(graph, pos, labels, font_size=5)
+    # draw nodes
+    for node in graph.nodes():
+        net.draw_networkx_nodes(graph, pos,
+                               nodelist=list(graph.nodes()),
+                               node_size=250, node_color='red', alpha=0.9)
+        print("drawn node:", node)
+    # draw edges
+    for edge in graph.edges:
+        net.draw_networkx_edges(graph, pos,
+                               edgelist=list(graph.edges()),
+                               width=3, alpha=0.9, edge_color='grey')
+        print("drawn edge:", edge)
+    # draw network labels
+    labels={}
+    for node in graph.nodes():
+        # label according to PDF problem statement node labeling standard
+        if str(node) != 'win':
+            labels[node]=str(node[0]+1)+", "+str(node[1]+1)
+        else:
+            labels['win'] = 'win'
+    net.draw_networkx_labels(graph, pos, labels, font_size=5)
     
-#     plt.axis('off')
-#     plt.show()
+    plt.axis('off')
+    plt.show()
 
 def input_maze():
     # read in number of nodes and edges
@@ -91,57 +91,6 @@ def input_maze():
                 else:
                     state_graph.add_node((rocket, lucky))
 
-    # def check_lucky(current_state, next_node):
-    #     if in_maze.edges()[(current_state[0], next_node)]['color'] == in_maze.nodes()[current_state[1]]['color']:
-    #         # move lucky if same color
-    #         next_state = (next_node, current_state[1])
-    #         # add to graph
-    #         # print("add state:", next_state)
-    #         state_graph.add_node(next_state)
-    #         # check in matrix
-    #         if state_matrix[next_state[0]][next_state[1]] == -1:
-    #             # print("add edge between", current_state, "and", next_state)
-    #             state_graph.add_edge(current_state, next_state)
-    #         next_state_check(next_state)
-
-    # def check_rocket(current_state, next_node):
-    #     if in_maze.edges()[(current_state[1], next_node)]['color'] == in_maze.nodes()[current_state[0]]['color']:
-    #         # move rocket if same color
-    #         next_state = (current_state[0], next_node)
-    #         # add to graph
-    #         # print("add state:", next_state)
-    #         state_graph.add_node(next_state)
-    #         # check in matrix
-    #         if state_matrix[next_state[0]][next_state[1]] == -1:
-    #             # print("add edge between", current_state, "and", next_state)
-    #             state_graph.add_edge(current_state, next_state)
-    #         next_state_check(next_state)
-
-    # def next_state_check(current_state):
-    #     if state_matrix[current_state[0]][current_state[1]] == -1:
-    #         state_matrix[current_state[0]][current_state[1]] = current_state
-    #         for next_lucky_move in list(in_maze.adj.items())[current_state[0]][1]:
-    #             check_lucky(current_state, next_lucky_move)
-    #         for next_rocket_move in list(in_maze.adj.items())[current_state[1]][1]:
-    #             check_rocket(current_state, next_rocket_move)
-    #     elif current_state[0] == len(color_map) or current_state[1] == len(color_map):
-    #             state_graph.add_edge(current_state, win_state)
-
-    # current_state = starting_state
-    # next_state_check(current_state)
-
-    # correct_order = [starting_state] + [to_node for from_node, to_node in net.bfs_edges(state_graph, starting_state)]
-    # out_str = ""
-    # for idx in range(len(correct_order)):
-    #     if idx+1 < len(correct_order):
-    #         if correct_order[idx][0] != correct_order[idx+1][0]:
-    #             out_str = out_str + 'R' + str(correct_order[idx+1][0]+1)
-    #             # print("rocket move to", correct_order[idx+1][0] + 1)
-            
-    #         if correct_order[idx][1] != correct_order[idx+1][1]:
-    #             out_str = out_str + 'L' + str(correct_order[idx+1][0]+1)
-    #             # print("lucky move to", correct_order[idx+1][1] + 1)
-    # print(out_str)
     # drawRefurbishedGraph(state_graph, net.fruchterman_reingold_layout(state_graph))
     
 input_maze()
